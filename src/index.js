e0e7421936dd453795ffc28d67e0b1668e840f10
CyrusCheung <m19990328@gmail.com>
2024-12-18 17:34:58 +0800

    tic tac toe completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
-function HelloWorld() {
-  return <h1 className="greeting">Hello, world!</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <App />
   </React.StrictMode>
 );
 
