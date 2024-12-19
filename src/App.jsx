1a7ac03d094591bd0bcbd9b708502c4f09b23c09
CyrusCheung <m19990328@gmail.com>
2024-12-19 17:10:47 +0800

    custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 import "@mantine/core/styles.css";
-import { createTheme, MantineProvider } from "@mantine/core";
+import {
+  createTheme,
+  MantineProvider,
+  AppShell,
+  Tooltip,
+  ActionIcon,
+} from "@mantine/core";
+import { IconPhone } from "@tabler/icons-react";
+
+import classes from "./App.module.css";
 
 const theme = createTheme({
   fontFamily: "Montserrat, sans-serif",
-  defaultRadius: 'md',
 });
 
 function App() {
-  return <MantineProvider theme={theme}>Hello</MantineProvider>;
+  console.log(classes);
+  return (
+    <MantineProvider theme={theme}>
+      <AppShell navbar={{ width: 100 }} padding="md">
+        <AppShell.Navbar
+          style={{ display: "flex", alignItems: "center" }}
+          p="md"
+        >
+          Navbar
+          <Tooltip label="Project" position="right">
+            <ActionIcon className={classes["action-icon"]} variant="filled">
+              <IconPhone className={classes["nav-icon"]} />
+            </ActionIcon>
+          </Tooltip>
+        </AppShell.Navbar>
+        <AppShell.Main>Main</AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  );
 }
 
 export default App;
